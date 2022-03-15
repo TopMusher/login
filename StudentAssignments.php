78973884075950be0befd4776cdd26f11b3e91b5
TopMusher <93521458+TopMusher@users.noreply.github.com>
2022-03-15 14:23:55 +0000

    StudentAssignments.php

---
+++ StudentAssignments.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+    <title>I HATE VIRTUAL MACHINES</title>
+            <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">GibJohn Tutoring</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="homepage.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Content <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Childrens Tutoring</a></li>
+          <li><a href="#">New Content</a></li>
+          <li><a href="#">Feedback from you</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Accessibility</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="sign_up.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="log_in.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav> 
+
+<!--PROGRESS BAR-->
+<div class="container">
+  <h4><center>Current Task Progress</center></h4>
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+      <span class="sr-only">70% Complete</span>
+    </div>
+  </div>
+</div>
+</body>
+</hmtl>
