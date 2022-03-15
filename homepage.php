42772ebae212c8d47cb582174583435cac19558f
TopMusher <93521458+TopMusher@users.noreply.github.com>
2022-03-15 14:19:19 +0000

    homepage.php

---
+++ homepage.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html5>
+<hmtl>
+        <head>
+            <title>test</title>
+            <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">GibJohn Tutoring</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="homepage.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Content <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Childrens Tutoring</a></li>
+          <li><a href="#">New Content</a></li>
+          <li><a href="#">Feedback from you</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Accessibility</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="sign_up.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="log_in.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="mainpage1">
+    <hr class="solid">
+
+        <img src="filler_img.JPG" width="30%" height="50%" align="left" padding="15px" border="1px solid black"/>
+        
+        <!-- adding the text area beside the main image-->
+        <p><h4>Lorem ipsum dolor sit amet, consectetur elit. Curabitur auctor  tellus, eget semper dolor volutpat commodo. Pellentesque vel malesuada odio, a fringilla elit. Sed mollis, purus in luctus porttitor, massa lacus commodo ex, convallis consectetur enim urna sit amet dolor. Vestibulum neque nunc, blandit ut ex at, venenatis porttitor ex. Aenean iaculis est vitae magna placerat fringilla. Cras maximus erat vel nisi viverra dapibus. Mauris vel lacus nec quam volutpat dignissim. Praesent consectetur nulla id tortor elementum, in imperdiet massa faucibus. Vestibulum vestibulum sem elit. Praesent a nisi id nibh commodo condimentum eget auctor mi. Morbi eget dictum metus. Vivamus id semper sapien. Nunc volutpat in dui at suscipit. Nam mattis at risus sed lobortis. 
+
+In facilisis lorem at sagittis euismod. Pellentesque  varius enim,  vehicula risus. Aenean ultricies  eget eros tempus pharetra. Etiam vulputate purus consequat consequat hendrerit. Nunc id sollicitudin ex. Nulla mi elit, laoreet eu mauris sit amet, malesuada feugiat est. Integer ac faucibus felis, vitae gravida metus. Nullam malesuada cursus est, ut ultricies quam tincidunt vel. Vivamus mollis. 
+
+        </h4></p>
+
+        <p><h2><center>All new Reward System!</center></h2></p>
+
+        <!--adding in the second 'sign up' button-->
+
+        
+        <a href="sign_up.php" class="button1" href="homepage.css">Sign up!</a>
+        
+       <p><h3>Lorem ipsum dolor sit amet, consectetur adipiscing. Curabitur auctor  tellus, eget  dolor volutpat commodo. Pellentesque vel odio, a fringilla elit. Sed mollis, purus in luctus porttitor, massa lacus commodo ex, convallis consectetur enim urna sit amet dolor. Vestibulum neque nunc, blandit ut ex at, venenatis porttitor ex. Aenean iaculis est vitae magna placerat fringilla. Cras maximus erat vel nisi viverra dapibus. Mauris vel lacus nec quam volutpat dignissim. Praesent consectetur nulla id tortor elementum, in imperdiet massa faucibus. Vestibulum vestibulum sem elit. Praesent a nisi id nibh commodo condimentum eget auctor mi. Morbi eget dictum metus. Vivamus id semper sapien. Nunc volutpat in dui at suscipit. Nam mattis at risus sed lobortis. 
+
+In facilisis lorem at sagittis euismod. Pellentesque et  enim, vel vehicula risus. Aenean ultricies dolor eget eros tempus pharetra. Etiam vulputate purus consequat consequat hendrerit. Nunc id sollicitudin ex. Nulla mi elit, laoreet eu mauris sit amet, malesuada feugiat est. Integer ac faucibus felis, vitae gravida metus. Nullam malesuada cursus est, ut ultricies quam tincidunt vel. Vivamus mollis. 
+</h3></p>
+
+<hr class="solid">
+
+<!--adding the second row of images below the text above-->
+
+
+
+</div>   
+</body>
+</hmtl>
