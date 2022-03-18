68f7545ef09a6deadcd8e52388a56c3fc68be3ac
TopMusher <93521458+TopMusher@users.noreply.github.com>
2022-03-18 13:22:53 +0000

    Update studentDashboard.php

--- studentDashboard.php
+++ studentDashboard.php
@@ -5,6 +5,7 @@
             <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <!--progress bar bootstrap-->
@@ -65,7 +66,32 @@
 <!--task options-->
 
 <div class="fillerImg1">
-  <img src="filler_img.JPG">
+    <div class="engLang">
+    <img src="https://th.bing.com/th/id/R.79f5bd9f018686f6ed1d0ce92c1cb3ea?rik=bI1n4Fs%2bCnz%2fgA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-3PJv4GHtwRY%2fUoxX2d_Y14I%2fAAAAAAAAAOY%2fJnDabPs3VcQ%2fs1600%2f15085325-learn-english-book-for-studying-a-foreign-language.jpg&ehk=ZghyZkXVQi%2fFh3Y599AgPhjsbU5I73SLlARf5kcQjr4%3d&risl=&pid=ImgRaw&r=0" width=250px float=left>
+    </div>
+
+    <div class="engLit">
+      <img src="https://th.bing.com/th/id/R.79f5bd9f018686f6ed1d0ce92c1cb3ea?rik=bI1n4Fs%2bCnz%2fgA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-3PJv4GHtwRY%2fUoxX2d_Y14I%2fAAAAAAAAAOY%2fJnDabPs3VcQ%2fs1600%2f15085325-learn-english-book-for-studying-a-foreign-language.jpg&ehk=ZghyZkXVQi%2fFh3Y599AgPhjsbU5I73SLlARf5kcQjr4%3d&risl=&pid=ImgRaw&r=0" width="250px" float=right>
+      <h4><center>English Language</center></h4><h4><center>English Literature</center></h4>
+    </div>
+</div>
+
+
+<div class="fillerImg1">
+  <img src="https://th.bing.com/th/id/OIP.AUYgY0H87fmqYGTCjtPKegHaHa?pid=ImgDet&rs=1" width=250px>
+  <h4><center>Mathematics</center></h4>
+</div>
+
+
+<div class="fillerImg1">
+  <img src="https://th.bing.com/th/id/OIP.zcb3ry6k2__n0pS9HX_JXwHaKr?pid=ImgDet&rs=1" width=250px>
+  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Science <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Biology</a></li>
+          <li><a href="#">Chemistry</a></li>
+          <li><a href="#">Pyhsics</a></li>
+        </ul>
+      </li>
 </div>
 <script>
 function on() {
