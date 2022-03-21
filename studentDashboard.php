c8afae875fe0ed24d403021f051276b36ee4d21b
TopMusher <93521458+TopMusher@users.noreply.github.com>
2022-03-21 12:28:44 +0000

    Update studentDashboard.php

--- studentDashboard.php
+++ studentDashboard.php
@@ -1,15 +1,66 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>test</title>
+    <title>Student Dashboard</title>
             <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <!--progress bar bootstrap-->
-  
+  <style> 
+    .icon-bar {
+    position: fixed;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+}
+
+.icon-bar a {
+    display: block;
+    text-align: center;
+    padding: 16px;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 20px;
+}
+
+.icon-bar a:hover {
+    background-color: #000;
+}
+
+.facebook {
+    background: #3B5998;
+    color: white;
+}
+
+.twitter {
+    background: #55ACEE;
+    color: white;
+}
+
+.google {
+    background: #dd4b39;
+    color: white;
+}
+
+.linkedin {
+    background: #007bb5;
+    color: white;
+}
+
+.youtube {
+    background: #bb0000;
+    color: white;
+}
+
+.content {
+    margin-left: 75px;
+    font-size: 30px;
+}
+  </style>
 </head>
 <body>
 
@@ -19,7 +70,7 @@
       <a class="navbar-brand" href="#">GibJohn Tutoring</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="homepage.php">Home</a></li>
+      <li class=""><a href="homepage.php">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Content <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Your Content</a></li>
@@ -47,6 +98,8 @@
   </div>
 </nav>
 
+
+
 <div class="container">
   <h4><center>Current Task Progress</center></h4>
   <div class="progress">
@@ -64,43 +117,119 @@
 </div>
 
 <!--task options-->
+<table style="margin:0 auto">
+  <tr>
+      <td>
+        <div class="fillerImg1">
+          <div class="engLang">
+          <div id="myModal" class="modal">
 
-<div class="fillerImg1">
-    <div class="engLang">
-    <img src="https://th.bing.com/th/id/R.79f5bd9f018686f6ed1d0ce92c1cb3ea?rik=bI1n4Fs%2bCnz%2fgA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-3PJv4GHtwRY%2fUoxX2d_Y14I%2fAAAAAAAAAOY%2fJnDabPs3VcQ%2fs1600%2f15085325-learn-english-book-for-studying-a-foreign-language.jpg&ehk=ZghyZkXVQi%2fFh3Y599AgPhjsbU5I73SLlARf5kcQjr4%3d&risl=&pid=ImgRaw&r=0" width=250px float=left>
-    </div>
+          <!-- Modal content -->
+          <div class="modal-content">   
+            <span class="close">&times;</span>
+            <h2>First English Language assessment:</h2>
+          </div>
 
-    <div class="engLit">
-      <img src="https://th.bing.com/th/id/R.79f5bd9f018686f6ed1d0ce92c1cb3ea?rik=bI1n4Fs%2bCnz%2fgA&riu=http%3a%2f%2f3.bp.blogspot.com%2f-3PJv4GHtwRY%2fUoxX2d_Y14I%2fAAAAAAAAAOY%2fJnDabPs3VcQ%2fs1600%2f15085325-learn-english-book-for-studying-a-foreign-language.jpg&ehk=ZghyZkXVQi%2fFh3Y599AgPhjsbU5I73SLlARf5kcQjr4%3d&risl=&pid=ImgRaw&r=0" width="250px" float=right>
-      <h4><center>English Language</center></h4><h4><center>English Literature</center></h4>
-    </div>
+</div>
+          <a id="myBtn">
+          <img src="https://maxcdn.icons8.com/Share/icon/ios7/Files/open_book1600.png" href="#" width=150px float=left>
+          <h4><center>English Language</center></h4>
+            </a>  
+        </div>
+      </td>
+
+    <td>
+      <div class="fillerImg1">
+        <a href="#">
+        <img src="https://th.bing.com/th/id/R.ca0f6a4cae066e86e621cf4a4c2dfda5?rik=cU%2b8pHEtJymUzQ&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_323457.png&ehk=91YEMY56QJorGF5v14MXmXm57FUD3%2bTmmdM5JsVsUBE%3d&risl=&pid=ImgRaw&r=0" width=150px height="150px" float=left>
+        <h4><center>English Literature</center></h4>
+        </a>  
+      </div>
+    </td>
+    <td>
+      <div class="fillerImg1">
+      <a href="#">
+        <img src="https://cdn4.iconfinder.com/data/icons/academic-disciplines-color/64/poetry-512.png"  width=150px float=left>
+        <h4><center>Poetry</center></h4>
+      </a>
+      </div>
+    </td>
+</tr>
 </div>
 
+</table>
 
 <div class="fillerImg1">
-  <img src="https://th.bing.com/th/id/OIP.AUYgY0H87fmqYGTCjtPKegHaHa?pid=ImgDet&rs=1" width=250px>
+<a href="#" id=">
+  <img src="https://cdn3.iconfinder.com/data/icons/education-ultimate-set/512/algebra-512.png" width=150px>
   <h4><center>Mathematics</center></h4>
+</a>
 </div>
 
+<table style="margin:0 auto">
+  <tr>
+    <td>
+      <div class="fillerImg1">
+      <a href="#">
+        <img src="https://th.bing.com/th/id/OIP.u3D42LiNnRVw9NQsk7WSdgHaIb?pid=ImgDet&rs=1" width=150px>
+        <h4><center>Physics</center></h4>
+      </a>
+      </div>
+    </td>
+    <td>
+      <div class="fillerImg1">
+      <a href="#">
+        <img src="https://cdn.onlinewebfonts.com/svg/img_490832.png" width=150px>
+        <h4><center>Chemistry</center></h4>
+      </a>      
+      </div>
+    </td>     
+    <td>
+      <div class="fillerImg1">
+      <a href="#">
+        <img src="https://th.bing.com/th/id/OIP.kL_ef35dVUKzXmYWRPUIiQHaHa?pid=ImgDet&rs=1" width=150px>
+        <h4><center>Physics</center></h4>
+      </a>  
+      </div>
+      </td>
+  </tr>
+  
+</table>
 
-<div class="fillerImg1">
-  <img src="https://th.bing.com/th/id/OIP.zcb3ry6k2__n0pS9HX_JXwHaKr?pid=ImgDet&rs=1" width=250px>
-  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Science <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Biology</a></li>
-          <li><a href="#">Chemistry</a></li>
-          <li><a href="#">Pyhsics</a></li>
-        </ul>
-      </li>
-</div>
 <script>
-function on() {
-  document.getElementById("overlay").style.display = "block";
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 </script>
+
+<div class="icon-bar">
+  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
+  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
+  <a href="#" class="google"><i class="fa fa-google"></i></a> 
+  <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+  <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
+</div>
 </body>
 </html>
