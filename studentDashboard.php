f864e89cbde48ce952629c0a077a7e4ab2e8b74d
TopMusher <93521458+TopMusher@users.noreply.github.com>
2022-03-23 10:40:31 +0000

    Update studentDashboard.php

--- studentDashboard.php
+++ studentDashboard.php
@@ -73,7 +73,7 @@
       <li class=""><a href="homepage.php">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Content <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Your Content</a></li>
+          <li><a href="studentContent.php">Your Content</a></li>
           <li><a href="#">New Content</a></li>
           <li><a href="#">Feedback from you</a></li>
         </ul>
@@ -103,8 +103,8 @@
 <div class="container">
   <h4><center>Current Task Progress</center></h4>
   <div class="progress">
-    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
-      <span class="sr-only">70% Complete</span>
+    <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+      <span class="sr-only">30% Complete</span>
     </div>
   </div>
 </div>
@@ -117,7 +117,7 @@
 </div>
 
 <!--task options-->
-<table style="margin:0 auto">
+<table style="margin: 60px; ">
   <tr>
       <td>
         <div class="fillerImg1">
@@ -127,9 +127,19 @@
           <!-- Modal content -->
           <div class="modal-content">   
             <span class="close">&times;</span>
-            <h2>First English Language assessment:</h2>
+            <h2>English Language assessment:</h2>
+            <p>Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+            <input type="textbox"/>
+            <p>Question 2: Lorem ipsum dolor sit amet, consectetur adipiscing?</p>
+            <input type="textbox"/>
+            <p>Question 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit.?</p>
+            <input type="textbox"/>
+            <p>Question 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac.?</p>
+            <input type="textbox"/>
+            <p>Question 5: Lorem ipsum dolor sit amet.?</p>
+            <input type="textbox"/>
+            <p><button type="submit" style=margin-top:10px>Submit</button></p>
           </div>
-
 </div>
           <a id="myBtn">
           <img src="https://maxcdn.icons8.com/Share/icon/ios7/Files/open_book1600.png" href="#" width=150px float=left>
@@ -140,15 +150,57 @@
 
     <td>
       <div class="fillerImg1">
-        <a href="#">
+        
+      <div id="myModal1" class="modal">
+
+<!-- Modal content -->
+      <div class="modal-content">   
+        <span class="close">&times;</span>
+        <h2>English Literature assessment:</h2>
+        <p>Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+        <input type="textbox"/>
+        <p>Question 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+        <input type="textbox"/>
+        <p>Question 3: Lorem ipsum dolor amet, consectetur adipiscing elit?</p>
+        <input type="textbox"/>
+        <p>Question 4: Lorem ipsum dolor sit amet,  elit?</p>
+        <input type="textbox"/>
+        <p>Question 5: Lorem ipsum  sit amet, consectetur adipiscing elit?</p>
+        <input type="textbox"/>
+        <p><button type="submit" style=margin-top:10px>Submit</button></p>
+      </div>
+      </div>
+<a id="myBtn2">
         <img src="https://th.bing.com/th/id/R.ca0f6a4cae066e86e621cf4a4c2dfda5?rik=cU%2b8pHEtJymUzQ&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_323457.png&ehk=91YEMY56QJorGF5v14MXmXm57FUD3%2bTmmdM5JsVsUBE%3d&risl=&pid=ImgRaw&r=0" width=150px height="150px" float=left>
         <h4><center>English Literature</center></h4>
-        </a>  
+</a>
+        
       </div>
     </td>
+
+
     <td>
       <div class="fillerImg1">
-      <a href="#">
+      <div id="myModal2" class="modal">
+
+<!-- Modal content -->
+      <div class="modal-content">   
+        <span class="close">&times;</span>
+        <h2>English Literature assessment:</h2>
+        <p>Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus quis metus.?</p>
+        <input type="textbox"/>
+        <p>Question 2: Lorem ipsum dolor sit.?</p>
+        <input type="textbox"/>
+        <p>Question 3: Lorem ipsum dolor amet, consectetur adipiscing elit?</p>
+        <input type="textbox"/>
+        <p>Question 4: Lorem ipsum dolor sit amet,  elit?</p>
+        <input type="textbox"/>
+        <p>Question 5: Lorem ipsum dolor sit amet, consectetur.?</p>
+        <input type="textbox"/>
+        <p><button type="submit" style=margin-top:10px>Submit</button></p>
+      </div>
+      </div>
+<a id="myBtn3">
         <img src="https://cdn4.iconfinder.com/data/icons/academic-disciplines-color/64/poetry-512.png"  width=150px float=left>
         <h4><center>Poetry</center></h4>
       </a>
@@ -156,12 +208,38 @@
     </td>
 </tr>
 </div>
+<td>
 
+  
 </table>
+   <div class="youtubeVid">
+  <iframe width="250" style="margin-right: 60px" src="https://www.youtube.com/embed/qSbpz7NmIEg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 
 <div class="fillerImg1">
-<a href="#" id=">
-  <img src="https://cdn3.iconfinder.com/data/icons/education-ultimate-set/512/algebra-512.png" width=150px>
+           
+        
+      </div>
+      <div id="myModal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">   
+  <span class="close">&times;</span>
+  <h2>English Language assessment:</h2>
+  <p>Question 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+  <input type="textbox"/>
+  <p>Question 2: Lorem ipsum dolor sit amet, consectetur adipiscing?</p>
+  <input type="textbox"/>
+  <p>Question 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit.?</p>
+  <input type="textbox"/>
+  <p>Question 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat ac.?</p>
+  <input type="textbox"/>
+  <p>Question 5: Lorem ipsum dolor sit amet.?</p>
+  <input type="textbox"/>
+  <p><button type="submit" style=margin-top:10px>Submit</button></p>
+</div>
+</div>
+  <img src="https://cdn3.iconfinder.com/data/icons/education-ultimate-set/512/algebra-512.png" width=150px float=left>
   <h4><center>Mathematics</center></h4>
 </a>
 </div>
@@ -196,6 +274,8 @@
   
 </table>
 
+
+
 <script>
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -220,6 +300,60 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+//code for the second modal form, cannot use the same one multiple times
+
+var modal1 = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal1.style.display = "none";
+  }
+}
+
+//third modal
+
+var modal2 = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+ 
+btn.onclick = function() {
+  modal2.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal2.style.display = "none";
   }
 }
 </script>
